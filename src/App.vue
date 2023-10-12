02de2f50e6d1d28ff653d798a735d613e5636a41
Reina Ben <rlyn@Reinas-MacBook-Air.local>
2023-10-12 06:02:17 -0700

    update objective

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <h5>a software engineer specializing in front-end</h5>
     </div>
     <p>
-      I’m a software engineer specializing in building (and occasionally designing) digital experiences. Currently, I’m focused on building internal apps, extending features, refactoring, and rewriting code on existing applications used by employees at Switch.
+      I’m a software engineer specializing in front-end with an objective of fullstack development. Currently, I’m focused on building internal apps, extending features, refactoring, and rewriting code on existing applications used by employees at Switch and have gained about 6 years working in a fast-paced collaborative team.
     </p>
 
     <h5 class="skills-title">
