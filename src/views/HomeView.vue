c8c004d20b779d58004ba809b821ffc18e75266c
Reina Ben <rlyn@Reinas-MacBook-Air.local>
2023-10-18 08:31:29 -0700

    update skills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="skills-tracker prose">
     <div class="skills">
-      <h4>Coding/Foundation</h4>
+      <h4>Programming</h4>
       <div
         v-for="(skill, sIndex) in coding_skills"
         :key="'codingskills-'+sIndex"
@@ -15,7 +15,7 @@
     </div>
 
     <div class="skills">
-      <h4>Front-end</h4>
+      <h4>Front-End</h4>
       <div
         v-for="(skill, sIndex) in frontend_skills"
         :key="'frontendskills-'+sIndex"
@@ -29,9 +29,23 @@
     </div>
 
     <div class="skills">
-      <h4>Back-end/Tools</h4>
+      <h4>Back-End</h4>
       <div
         v-for="(skill, sIndex) in backend_skills"
+        :key="'backendskills-'+sIndex"
+        class="item"
+      >
+        <h5>{{ skill.name }}</h5>
+        <!--
+        <progress v-if="skill.progress" class="progress progress-primary" :value="skill.progress" max="100"></progress>
+        -->
+      </div>
+    </div>
+
+    <div class="skills">
+      <h4>Deployment Pipeline</h4>
+      <div
+        v-for="(skill, sIndex) in deployment_skills"
         :key="'backendskills-'+sIndex"
         class="item"
       >
@@ -51,36 +65,41 @@ export default {
     return {
       coding_skills: [
         { name: 'JavaScript ES6', progress: '100' },
-        { name: 'HTML5', progress: "100" },
+        { name: 'Typescript', progress: "100" },
         { name: 'CSS3', progress:"100" },
-        { name: 'UI Design System', progress: '30' },
-        { name: 'System Design', progress: '60' },
-        { name: 'Data Structure & Algorithm', progress: '40' }
+        { name: 'PostCSS', progress: '30' },
+        { name: 'HTML5', progress: '60' },
+        { name: 'System Design' },
+        { name: 'Data Structure & Algorithm', progress: '40' },
+        { name: 'PHP' },
+        { name: 'Coffeescript' },
+        { name: 'Ruby' },
+        { name: 'Golang' }
       ],
       frontend_skills: [
         { name: 'Vue3', progress: '100' },
-        { name: 'Reactjs', progress: '30' },
         { name: 'Emberjs', progress: '30' },
+        { name: 'Tailwind/DaisyUI', progress: '30' },
         { name: 'Nuxt3', progress: '70' },
         { name: 'TailwindCSS/DaisyUI', progress: '90' },
-        { name: 'Typescript', progress: '70' },
-        { name: 'PostCSS', progress: "50" },
-        { name: 'Storybook', progress: '30' },
-        { name: 'Quasar Framework', progress: '80' },
-        { name: 'Vuetify', progress: '50' },
-        { name: 'Grav', progress: '50' },
+        { name: 'Quasar Framework', progress: '70' },
+        { name: 'Grav', progress: "50" },
+        { name: 'Zurb Foundation', progress: '30' },
+        { name: 'Storybook', progress: '80' }
       ],
       backend_skills: [
         { name: 'Laravel', progress: "50"},
-        //{ name: 'Mongodb', progress: '30' },
-        { name: 'Apollo GraphQL', progress: '50' },
-        { name: 'Commandline / Git / Vim', progress: '80' },
-        { name: 'Netlify & Serverless Functions', progress: '50' },
-        { name: 'DockerCLI' },
-        { name: 'Rancher/Nerctrl' },
-        { name: 'Jenkins' },
-        //{ name: 'FaunaDB' },
-      ]
+        { name: 'Wordpress (Monolith architecture)', progress: '30' }
+      ],
+      deployment_skills: [
+        { name: 'Git', progress: "50"},
+        { name: 'Gitlab', progress: '30' },
+        { name: 'Docker CLI', progress: '30' },
+        { name: 'Rancher/Nerdctl CLI', progress: '30' },
+        { name: 'Jenkins', progress: '30' },
+        { name: 'Bash', progress: '30' },
+        { name: 'Vim' }
+      ],
     }
   },
 }
